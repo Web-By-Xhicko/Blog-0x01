2f7b5b49524a50294d0ed371498cbbadab9f30fa
Xhicko <otagadaisaac@gmail.com>
2023-04-24 11:08:13 +0100

    Fixed Dom switching issues

--- blogApp/static/js/Index.js
+++ blogApp/static/js/Index.js
@@ -1,15 +1,20 @@
-let subProfile = document.getElementById('Sub-Profile')
-function toggleMenu(){
-    subProfile.classList.toggle('Open-Profile')
-}
-
-let openCategory = document.getElementById('Open-Category')
-function toggleMenu2(){
-    openCategory.classList.toggle('Open-Category')
-}
-
-
 let revOpts = document.getElementById('RevOpts')
 function RevOpts(){
     revOpts.classList.toggle('active')
 }
+
+
+let moreUrl = document.querySelector('.More_Url')
+let sideProfile = document.querySelector('.SideProfile')
+let openCategory = document.querySelector('.OpenCategoryList')
+let categoryList = document.querySelector('.CategoryList')
+
+moreUrl.addEventListener('click', ()=>{
+    sideProfile.classList.toggle('OpenSideProfile')
+    categoryList.classList.remove('CatList')
+  })
+
+openCategory.addEventListener('click', ()=>{
+    categoryList.classList.toggle('CatList')
+    sideProfile.classList.remove('OpenSideProfile')
+})
