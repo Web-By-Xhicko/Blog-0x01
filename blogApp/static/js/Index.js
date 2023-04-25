a6aa6467729db70a3726a210b02a31bdf5f99348
Xhicko <otagadaisaac@gmail.com>
2023-04-25 09:46:53 +0100

    More Updates on Category list, responsive 768px

--- blogApp/static/js/Index.js
+++ blogApp/static/js/Index.js
@@ -1,13 +1,10 @@
-let revOpts = document.getElementById('RevOpts')
-function RevOpts(){
-    revOpts.classList.toggle('active')
-}
-
-
 let moreUrl = document.querySelector('.More_Url')
 let sideProfile = document.querySelector('.SideProfile')
 let openCategory = document.querySelector('.OpenCategoryList')
 let categoryList = document.querySelector('.CategoryList')
+let openCategory2 = document.querySelector('.OpenCategoryList2')
+let categoryList2 = document.querySelector('.CategoryList2')
+
 
 moreUrl.addEventListener('click', ()=>{
     sideProfile.classList.toggle('OpenSideProfile')
@@ -18,3 +15,9 @@ openCategory.addEventListener('click', ()=>{
     categoryList.classList.toggle('CatList')
     sideProfile.classList.remove('OpenSideProfile')
 })
+
+openCategory2.addEventListener('click', ()=>{
+    categoryList2.classList.toggle('CatList2')
+   
+})
+
