f9e63ce4dd0d67606b2148d8bd0e0035e3f8cb65
Xhicko <otagadaisaac@gmail.com>
2023-08-15 07:10:01 +0100

    Removed irrelevant fields in the comment forms and in templates

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -2,13 +2,7 @@ from django import forms
 from .models import Comment
 
 class NewCommentForm(forms.ModelForm):
-    # Name = forms.CharField(widget=forms.TextInput(attrs={'class':'input'
-    #                                                      , 'placeholder':'Username'}))
-    # Email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'input',
-    #                                                          'placeholder':'Email'}))
-    Comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':200 ,
-                                                            'class':'textarea',
-                                                              'placeholder':'Comment'}))
+    Comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':200 , 'class':'textarea', 'placeholder':'Comment'}))
 
     class Meta:
         model = Comment
