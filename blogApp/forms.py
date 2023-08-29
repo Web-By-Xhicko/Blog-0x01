2beae4db00bb44b999841d8bc839c1444b375752
Xhicko <otagadaisaac@gmail.com>
2023-08-29 16:05:51 +0100

    Profile update Responsiveness Start

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Comment
 
 class NewCommentForm(forms.ModelForm):
-    Comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':200 , 'class':'textarea', 'placeholder':'Comment'}))
+    Comment = forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':200 , 'class':'textarea','placeholder':'Comment'}))
 
     class Meta:
         model = Comment
